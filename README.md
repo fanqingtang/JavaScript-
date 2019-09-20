716b80f4e5ac0b411d92d9b151ac7ff9a310d8b0
fanqingtang <fanqingtang@lexue100.com>
2019-09-20 09:09:50 +0800

    '添加数组获取交集的方法'

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 这里实现一个原生的 `bind`方法
 
 ``` javascript
-
+  // 实现bind方法
   Function.prototype.bind = function (context) {
     let args = Array.prototype.slice.call(arguments, 1),
         self = this;
@@ -17,3 +17,40 @@
     return fBound;
   }
 ```
+  这里获取数组的交集的方法
+
+  ``` javascript
+    //获取数组得到的交集
+  let a = [1,2,3,4],
+      b = [2,3,4,5];
+    //获取交集的第一种方法
+    function intersection1 (a, b) {   
+      let result = a.concat(b).sort((a, b) => a - b);
+      let sameArr = result.filter((v, i) => result.indexOf(v) !== i);
+      return sameArr;
+    }
+    intersection(a, b);    
+    // 获取交集的第二种方法
+    function intersection2 (a, b) {  
+      let result = a.filter((v) => b.indexOf(v) > -1);
+      return result;
+    }
+
+    // 获取交集的第三种方法
+    function intersection (a, b) {
+      let result = [];
+      for (let i = 0; i < a.length; i++) {
+        let current = a[i];
+        if (b.indexOf(current) > -1) {
+          result.push(current);
+        }
+      }
+      for (let j = 0; j < result.length; j++) {
+        if (result[j] === result[j+1]) {
+          result.splice(j, 1);
+          j--;
+        }
+      }
+      return result;
+    }
+  ```
--- js/bind.js
+++ js/bind.js
@@ -1,28 +1,72 @@
-let obj = {
-  name: 'fqt',
-  age: 20
-};
-
-Function.prototype.bind = function (context) {
-  let args = Array.prototype.slice.call(arguments, 1);
-  let self = this;
-  function Fn () {}
-
-  let fBound =  function () {
-    let bindArgs = Array.prototype.slice.call(arguments);
-    return self.apply(this instanceof fBound ? this : context, args.concat(bindArgs));
+{
+  // bind 方法的实现
+  let obj = {
+    name: 'fqt',
+    age: 20
+  };
+  
+  Function.prototype.bind = function (context) {
+    let args = Array.prototype.slice.call(arguments, 1);
+    let self = this;
+    function Fn () {} //定义一个空函数用来继承原型上的方法
+  
+    let fBound =  function () {
+      let bindArgs = Array.prototype.slice.call(arguments);
+      return self.apply(this instanceof fBound ? this : context, args.concat(bindArgs));
+    }
+    Fn.prototype = this.prototype;
+    fBound.prototype = new Fn();
+    return fBound;
   }
-  Fn.prototype = this.prototype;
-  fBound.prototype = new Fn();
-  return fBound;
+  
+  function demo (adress, phone) {
+    return this.name + adress+phone;
+  }
+  
+  let adress = ' from 江西九江';
+  let phone = '13632444442';
+  let name = demo.bind(obj, adress);
+  let instance = new name();
+  // console.log(name(phone));
 }
+{
+  // 获得数组的 交集、并集、差集 的实现方法
+  let a = [1,2,3,4,4],
+      b = [2,3,4,5];
+  /**    
+    获取交集的第一种方法
+    function intersection (a, b) {   
+      let result = a.concat(b).sort((a, b) => a - b);
+      let sameArr = result.filter((v, i) => result.indexOf(v) !== i);
+      console.log(sameArr);
+    }
+    intersection(a, b);
+  */
 
-function demo (adress, phone) {
-  return this.name + adress+phone;
+  /** 
+   * 获取交集的第二种方法
+  function intersection (a, b) {  
+    let result = a.filter((v) => b.indexOf(v) > -1);
+    return result;
+  }
+  console.log(intersection(a, b));
+  */
+
+  function intersection (a, b) {
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+      let current = a[i];
+      if (b.indexOf(current) > -1) {
+        result.push(current);
+      }
+    }
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === result[j+1]) {
+        result.splice(j, 1);
+        j--;
+      }
+    }
+    return result;
+  }
+  console.log(intersection(a, b));
 }
-
-let adress = ' from 江西九江';
-let phone = '13632444442';
-let name = demo.bind(obj, adress);
-let instance = new name();
-// console.log(name(phone));
